578081342b36e12b2fa26c7e3195d4a8dce34e84
tishamahale <tishamahale@gmail.com>
2025-04-09 17:00:24 +0530

    Update datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,18 @@
+package java.new;
+
+public class datatypes {
+    public static void main(String[] args){
+        int a = 10;
+        byte b = 5;
+        short c = 2;
+        long d = 100;
+        double e = 0.04;
+        float f = 0.10f;
+        char g = 'a';
+        boolean y = true;
+        String z = "hello";
+        int arr[] = {1,2,3,4};
+        System.out.println("boolean: "+y);
+        System.out.println("String: "+z);
+        
+    }}
